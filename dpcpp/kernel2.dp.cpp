f67d4c44cfc945869bd8192abbbad7c969d91a05
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-01-30 11:07:37 -0500

    Removing unnecesary local accessor in k2.

--- dpcpp/kernel2.dp.cpp
+++ dpcpp/kernel2.dp.cpp
@@ -30,8 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 void
 gpu_sum_evals_kernel(
 	sycl::nd_item<3> item_ct1,
-	GpuData cData,
-	int *sSum_evals)
+	GpuData cData)
 // The GPU global function sums the evaluation counter states
 // which are stored in evals_of_new_entities array foreach entity,
 // calculates the sums for each run and stores it in evals_of_runs array.
@@ -53,11 +52,11 @@ gpu_sum_evals_kernel(
 	}
 	
 	// Perform warp-wise reduction
-	*sSum_evals = sycl::reduce_over_group(groupIdx, partsum_evals, std::plus<>());
+	partsum_evals = sycl::reduce_over_group(groupIdx, partsum_evals, std::plus<>());
 
 	if (threadIdx_x == 0)
 	{
-		cData.pMem_gpu_evals_of_runs[blockIdx_x] += *sSum_evals;
+		cData.pMem_gpu_evals_of_runs[blockIdx_x] += partsum_evals;
 	}
 }
 
@@ -68,8 +67,6 @@ void gpu_sum_evals(uint32_t blocks, uint32_t threadsPerBlock)
 		cData.init();
 		auto cData_ptr_ct1 = cData.get_ptr();
 
-		sycl::local_accessor<int, 0> sSum_evals_acc_ct1(cgh);
-
 		cgh.parallel_for(
 			sycl::nd_range<3>(
 				sycl::range<3>(1, 1, blocks) * sycl::range<3>(1, 1, threadsPerBlock),
@@ -78,8 +75,7 @@ void gpu_sum_evals(uint32_t blocks, uint32_t threadsPerBlock)
 			[=](sycl::nd_item<3> item_ct1) {
 				gpu_sum_evals_kernel(
 					item_ct1,
-					*cData_ptr_ct1,
-					sSum_evals_acc_ct1.template get_multi_ptr<sycl::access::decorated::no>().get()
+					*cData_ptr_ct1
 				);
 		});
 	});
