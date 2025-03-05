f6a984f79cafd0f32c01a701f21987eaca4e4404
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-03-05 15:53:53 +0100

    Removing already commented-out line.

--- dpcpp/dpcpp_migration.h
+++ dpcpp/dpcpp_migration.h
@@ -25,7 +25,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef DPCPP_MIGRATION_H
 #define DPCPP_MIGRATION_H
 
-//#define XeDeviceSynchronize()  dpct::get_current_device().queues_wait_and_throw();
 #define XeDeviceSynchronize() dpct::get_default_queue().wait_and_throw();
 
 #define cudaMemGetInfo(x, y) ad_MemGetInfo(x, y)
