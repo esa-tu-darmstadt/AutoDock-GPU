75632e14f68a031fdca522c719d6e92ced3f42a5
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-04-06 08:52:28 +0200

    Fixing size of local memory.

--- dpcpp/kernel_ad.dp.cpp
+++ dpcpp/kernel_ad.dp.cpp
@@ -466,7 +466,7 @@ void gpu_gradient_minAD(
 		/* Reduction using matrix units */
 		sycl::local_accessor</*sycl::half*/bf16, 1> data_to_be_reduced(sycl::range<1>(4 * threads), cgh);
 		sycl::local_accessor</*sycl::half*/bf16, 1> Q_data(sycl::range<1>(tM * tK), cgh);
-		sycl::local_accessor</*sycl::half*/float, 1> tmp(sycl::range<1>(16 * 16), cgh);
+		sycl::local_accessor</*sycl::half*/float, 1> tmp(sycl::range<1>(4 * threads), cgh);
 		/* Reduction using matrix units */
 		#endif
 
