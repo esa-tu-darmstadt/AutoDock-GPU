973047c7e8c58318af86103e789316e0f6389182
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-04-06 08:59:51 +0200

    Cleaning up a bit.

--- dpcpp/kernel_ad.dp.cpp
+++ dpcpp/kernel_ad.dp.cpp
@@ -75,9 +75,9 @@ gpu_gradient_minAD_kernel(
 	#ifdef USE_XMX
 	/* Reduction using matrix units */
 	,
-	/*sycl::half*/bf16 *data_to_be_reduced,
-	/*sycl::half*/bf16 *Q_data,
-	/*sycl::half*/float *tmp
+	bf16 *data_to_be_reduced,
+	bf16 *Q_data,
+	float *tmp
 	/* Reduction using matrix units */
 	#endif
 )
@@ -464,9 +464,9 @@ void gpu_gradient_minAD(
 
 		#ifdef USE_XMX
 		/* Reduction using matrix units */
-		sycl::local_accessor</*sycl::half*/bf16, 1> data_to_be_reduced(sycl::range<1>(4 * threads), cgh);
-		sycl::local_accessor</*sycl::half*/bf16, 1> Q_data(sycl::range<1>(tM * tK), cgh);
-		sycl::local_accessor</*sycl::half*/float, 1> tmp(sycl::range<1>(4 * threads), cgh);
+		sycl::local_accessor<bf16, 1> data_to_be_reduced(sycl::range<1>(4 * threads), cgh);
+		sycl::local_accessor<bf16, 1> Q_data(sycl::range<1>(tM * tK), cgh);
+		sycl::local_accessor<float, 1> tmp(sycl::range<1>(4 * threads), cgh);
 		/* Reduction using matrix units */
 		#endif
 
