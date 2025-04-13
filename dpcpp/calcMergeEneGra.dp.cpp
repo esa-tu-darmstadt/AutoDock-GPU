4fc3840950051587d0dcf2283d5b2d361cc14cd6
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-04-13 11:15:43 +0200

    Adding missing call to map_input_array().

--- dpcpp/calcMergeEneGra.dp.cpp
+++ dpcpp/calcMergeEneGra.dp.cpp
@@ -898,6 +898,24 @@ void gpu_calc_energrad(
 
 	#endif // DEBUG_XMX_INPUTS
 
+	#ifdef SET_PVC_SPECIFIC
+	map_input_array(
+		item_ct1,
+		#ifdef USE_GLOB_SPACE_XMX_INPUTS
+		data_to_be_reduced_global, // TODO: specify offset
+		data_to_be_reduced_global_arranged // TODO: specify offset
+		#else
+		data_to_be_reduced,
+		data_to_be_reduced_arranged
+		#endif
+		#ifdef DEBUG_XMX_INPUTS_INDEX_MAP
+		,
+		in_indexes,
+		out_indexes
+		#endif
+	);
+	#endif
+
 	/*
 	print_submatrix_WG<
 		bf16,
