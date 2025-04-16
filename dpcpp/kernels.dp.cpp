814034d5d31c4daa97e64ec55e404b2296412dfa
Leonardo Solis Vasquez <leonardosolis86@gmail.com>
2025-04-16 09:24:45 +0200

    Re-writing calls to joint_matrix_load() as later improvement is planned.

--- dpcpp/kernels.dp.cpp
+++ dpcpp/kernels.dp.cpp
@@ -364,7 +364,8 @@ void reduce_via_matrix_units (
 				#else
 				sycl::local_ptr<TA>(data_to_be_reduced + offset),
 				#endif
-				tM); // Col-major -> stride is tM
+				tM // Col-major -> stride is tM
+			);
 
 			joint_matrix_mad(sg, sub_V, sub_A, sub_P, sub_V);
 		}
@@ -388,10 +389,16 @@ void reduce_via_matrix_units (
 			sg,
 			sub_Q,
 			Q_data_mptr,
-			tM);	// Col-major -> stride is tM
+			tM // Col-major -> stride is tM
+		);
 		#else
 		fill_Q(item, Q_data);
-		joint_matrix_load(sg, sub_Q, sycl::local_ptr<TA>(Q_data), tM);	// Col-major -> stride is tM
+		joint_matrix_load(
+			sg,
+			sub_Q,
+			sycl::local_ptr<TA>(Q_data),
+			tM // Col-major -> stride is tM
+		);
 		#endif
 
 		// 2. Perform line sum: C <- QW + C (zero)
